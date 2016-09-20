a0a0e82b96aac087bd1d301d0257a58a331389e9
James Monger <jameskmonger@hotmail.co.uk>
2016-09-21 00:01:41 +0100

    Implement

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import * as ExtendableError from "es6-error";
+const ExtendableError = require("es6-error");
 
 export default class MissingDependencyError extends ExtendableError {
+    // these are here because es6-error has no typings
+    public name: string;
+    public message: string;
+
     constructor (dependencyName: string) {
         super(`Missing dependency: ${dependencyName}`);
     }
